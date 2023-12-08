0d57dd7574f6959e4c0c705add33dcb7d5772ca8
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-12-08 16:18:23 +0400

    implement search Logic, ToursPage and ToursList with TourCard

--- app/(dashboard)/tours/page.js
+++ app/(dashboard)/tours/page.js
@@ -1,7 +1,23 @@
 import React from "react";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import ToursPage from "@/components/ToursPage";
+import { getAllTours } from "@/utils/action";
 
-const ToursPage = () => {
-  return <div>ToursPage</div>;
+const AllToursPage = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["tours", ""],
+    queryFn: () => getAllTours(),
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ToursPage />
+    </HydrationBoundary>
+  );
 };
 
-export default ToursPage;
+export default AllToursPage;
---
+++ components/ToursPage.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { getAllTours } from "@/utils/action";
+import { useQuery } from "@tanstack/react-query";
+import ToursList from "./ToursList";
+import { useState } from "react";
+
+const TousPage = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const { data, isPending } = useQuery({
+    queryKey: ["tours", searchTerm],
+    queryFn: () => getAllTours(searchTerm),
+  });
+  return (
+    <>
+      <form className="max-w-lg mb-12">
+        <div className="join w-wull">
+          <input
+            type="text"
+            placeholder="enter city or country here"
+            className="input input-bordered join-item w-full"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            value={searchTerm}
+            required
+          />
+          <button
+            className="btn btn-primary join-item"
+            type="button"
+            disabled={isPending}
+            onClick={() => setSearchTerm("")}
+          >
+            {isPending ? "please wait..." : "Search"}
+          </button>
+        </div>
+      </form>
+      {isPending ? (
+        <span className="loading"></span>
+      ) : (
+        <ToursList data={data} />
+      )}
+    </>
+  );
+};
+
+export default TousPage;
